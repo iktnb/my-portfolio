fd4cad008bedd975ea8d1d443d24295517b6304d
Yuirii Basiuk <yuirii.basiuk@j-vers.com>
2026-03-01 02:23:32 +0000

    Add design-system submodule and remove existing components and documentation

--- design-system/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { Card, type CardProps } from './Card'
-export { GlowButton, type GlowButtonProps } from './GlowButton'
-export { SectionReveal, type SectionRevealProps } from './SectionReveal'
